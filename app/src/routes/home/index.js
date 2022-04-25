e9645764848b033c7d11045c5efb94387a5e6187
JeongUk <qjawls65@naver.com>
2022-04-25 17:14:33 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -13,5 +13,9 @@ router.get("/", ctrl.output.home);
 router.get("/login",ctrl.output.login);
 router.post("/login",ctrl.process.login);
 
+//register 화면
+router.get("/register",ctrl.output.register);
+
+
 
 module.exports = router;

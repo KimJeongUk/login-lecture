d4a0e0ed52cc915a41ba6cbc5167eddfca03e3e9
JeongUk <qjawls65@naver.com>
2022-04-27 17:39:31 +0900

    로그 관리 morgan 사용

--- app/app.js
+++ app/app.js
@@ -13,11 +13,12 @@
 // app.listen(3001, () =>{
 //     console.log("http로 가동된 서버");
 // });
-
+"use strict";
 //모듈
 const express = require("express");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); //환경변수
+const morgan = require("morgan"); // 로그
 dotenv.config();
 
 const app = express();
@@ -25,13 +26,17 @@ const app = express();
 // 라우팅
 const home = require("./src/routes/home");
 
+
+const accessLogStream = require("./src/config/log.js");
+
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); //url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 제대로 인식되지 않는 문제 해결
-
+app.use(morgan("common",{ stream : accessLogStream }));
+app.use(morgan("dev"));
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드
 
--- app/bin/www.js
+++ app/bin/www.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const app = require("../app");
+const app = require("../app.js");
 // 포트
 const PORT = process.env.PORT || 3000;
 
